a2a99486a212d070249d46cc48fcde28c7ed012b
Clayton Salem <claytonjsalem@gmail.com>
2025-09-15 10:59:21 -0400

    Hand prepend the correct path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	outDir: "docs",
-	base: "https://claytron5000.github.io/fcc-scraper/",
+	base: "./fcc-scraper/",
 });
